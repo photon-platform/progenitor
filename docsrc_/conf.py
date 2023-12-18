2baafe7e08a0a66730ab5d324d375b06406a5432
phiarchitect <phi@phiarchitect.com>
2023-12-18 05:30:59 -0800

    update docs

--- docsrc_/conf.py
+++ docsrc_/conf.py
@@ -1,12 +1,12 @@
 from ablog.conf import *
-import photon_platform.progenitor as module
+import photon_platform.logger as module
 version = module.__version__
-
 
 org = 'photon-platform'
 org_name = 'PHOTON platform'
 
-repo = 'progenitor'
-repo_name = 'progenitor'
+repo = 'photon-logger'
+repo_name = 'logger'
 
 setup_globals(org, org_name, repo, repo_name)
+
